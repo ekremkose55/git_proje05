7653b7ec34b3b20e356f8d00586f1ff086c03c31
ekremkose55 <67019372+ekremkose55@users.noreply.github.com>
2020-07-26 01:23:30 -0500

    Update FirstClass.java
    son degisiklikler

--- src/git_proje05/FirstClass.java
+++ src/git_proje05/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit local masterdan");
+		System.out.println("2. commit local masterdan");
+		System.out.println("3. commit local masterdan");
+		System.out.println("4. commit local masterdan");
 
 	}
 
